b051288c17caf73cb655ab8f23b0c00973d39af2
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-07 10:08:30 +0800

    initial app

---
+++ 1-comment/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>React 17!</div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
