cf1712b7203b119c17ceb7b421e998947c3dc3bd
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-10 18:12:36 +0800

    Update index.js

--- 2-seasons/src/index.js
+++ 2-seasons/src/index.js
@@ -2,18 +2,34 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [location, setLocation] = useState();
-    const [error, setError] = useState('');
-    
+    const [location, setLocation] = useState({
+        lat: null,
+        long: null
+    });
+    const [errorMessage, setErrorMessage] = useState('');
+
     window.navigator.geolocation.getCurrentPosition(
-        position => setLocation(position),
-        err => setError(err)
+        position => setLocation({
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        }),
+        err => setErrorMessage(err.message)
     );
 
     return (
         <div>
-            <h1>Latitude: {location?location.coords.latitude:null}</h1>
-            <h1>Longitude: {location?location.coords.longitude:null}</h1>
+            
+            {
+                errorMessage && !location.lat ?
+                    <h1>Error: {errorMessage}</h1>
+                : !errorMessage && location.lat ?
+                    <div>
+                        <h1>Latitude: {location.lat}</h1>
+                        <h1>Longitude: {location.long}</h1>
+                    </div>
+                :
+                    <h1>Loading...</h1>
+            }
         </div>
     );
 };
