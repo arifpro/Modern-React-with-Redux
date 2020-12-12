be02fd931f7e2b681bd03a530b6580009bed6ec4
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-12 15:02:59 +0800

    Update SearchBar.js

--- 3-pictures/src/components/SearchBar/SearchBar.js
+++ 3-pictures/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styles
 import useStyles from './stylesSearchBar';
 
 const SearchBar = () => {
     const styles = useStyles();
+    const [searchText, setSearchText] = useState('');
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(searchText);
+    };
 
     return (
         <div className="ui segment">
-            <form className="ui form">
+            <form className="ui form" onSubmit={onFormSubmit}>
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text"/>
+                    <input
+                        value={searchText}
+                        onChange={e => setSearchText(e.target.value)}
+                    />
                 </div>
             </form>
         </div>
