72de21f9266b241f56065c174ccfe6ef03f7c1c1
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-22 12:12:50 +0800

    style

--- 4-videos/src/components/VideoItem.js
+++ 4-videos/src/components/VideoItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './VideoItem.css'
 
 const VideoItem = ({ title, video, image }) => {
     return (
-        <div className="item">
-            <p>{title}</p>
-            <img src={image} alt={title}/>
+        <div className="video-item item">
+            <img className="ui image" src={image} alt={title}/>
+            <div className="content">
+                <div className="header">
+                    <p>{title}</p>
+                </div>
+            </div>
             {/* <video src={`https://youtube.com/watch?v=${video}`}></video> */}
         </div>
     )
